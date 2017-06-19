4553d1a682b50e9c2b7d0ff8fd113ab892867bb9
Steve Temple <steve@gibedigital.com>
2017-06-19 22:14:39 +0100

    Make the Ninject bindings implicit

--- Gibe.CacheBusting/UrlExtensions.cs
+++ Gibe.CacheBusting/UrlExtensions.cs
@@ -1,12 +1,21 @@
 ﻿using System.Web.Mvc;
+using Ninject;
 
 namespace Gibe.CacheBusting
 {
 	public static class UrlExtensions
 	{
+		private static IKernel _kernel;
+
+		private static IKernel Kernel()
+		{
+			return _kernel ?? 
+				(_kernel = new StandardKernel(new DefaultNinjectBindingsModule()));
+		}
+
 		public static string Asset(this UrlHelper url, string filename)
 		{
-			var manifest = DependencyResolver.Current.GetService<IRevisionManifest>();
+			var manifest = Kernel().Get<IRevisionManifest>();
 			return manifest.ContainsPath(filename) ? manifest.GetHashedPath(filename) : filename;
 		}
 	}
