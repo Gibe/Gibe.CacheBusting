33eabd96d250ff4c3bdae2b069dbf5bdb74800e7
jake williamson <jake.williamson@ribsdigital.com>
2019-03-12 12:38:17 +0000

    ninject bindings project

---
+++ Gibe.CacheBusting.Ninject/DefaultBindingsModule.cs
@@ -0,0 +1,17 @@
+﻿using Ninject.Modules;
+
+namespace Gibe.CacheBusting.Ninject
+{
+	public class DefaultBindingsModule : NinjectModule
+	{
+		public override void Load()
+		{
+			Bind<Gibe.CacheBusting.IRevisionManifest>().To<Gibe.CacheBusting.RevisionManifest>().InSingletonScope();
+			Bind<Gibe.CacheBusting.IManifestFileFactory>().To<Gibe.CacheBusting.ConfigManifestFileFactory>();
+			Bind<Gibe.CacheBusting.IManifestFileFactory>().To<Gibe.CacheBusting.ConfigManifestFileFactory>();
+			Bind<Gibe.FileSystem.IDirectoryService>().To<Gibe.FileSystem.DirectoryService>();
+			Bind<Gibe.FileSystem.IFileService>().To<Gibe.FileSystem.FileService>();
+			Bind<Gibe.FileSystem.IPaths>().To<Gibe.FileSystem.Paths>();
+		}
+	}
+}
