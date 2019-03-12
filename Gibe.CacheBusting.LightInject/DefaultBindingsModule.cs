9ea0a9a7682ec452569b7d6d2189c1576c87b540
jake williamson <jake.williamson@ribsdigital.com>
2019-03-12 12:38:55 +0000

    lightinject bindings project

---
+++ Gibe.CacheBusting.LightInject/DefaultBindingsModule.cs
@@ -0,0 +1,16 @@
+﻿using LightInject;
+
+namespace Gibe.CacheBusting.LightInject
+{
+	public static class DefaultBindingsModule
+	{
+		public static void RegisterServicesFactories(IServiceRegistry container)
+		{
+			container.Register<Gibe.CacheBusting.IRevisionManifest, Gibe.CacheBusting.RevisionManifest>();
+			container.Register<Gibe.CacheBusting.IManifestFileFactory, Gibe.CacheBusting.ConfigManifestFileFactory>();
+			container.Register<Gibe.FileSystem.IDirectoryService, Gibe.FileSystem.DirectoryService>();
+			container.Register<Gibe.FileSystem.IFileService, Gibe.FileSystem.FileService>();
+			container.Register<Gibe.FileSystem.IPaths, Gibe.FileSystem.Paths>();
+		}
+	}
+}
