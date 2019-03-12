689ae2856873d06476c3ff75d5854f1600eee708
jake williamson <jake.williamson@ribsdigital.com>
2019-03-12 12:39:41 +0000

    autofac bindings project

---
+++ Gibe.CacheBusting.Autofac/DefaultBindingsModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+
+namespace Gibe.CacheBusting.Autofac
+{
+	public class DefaultBindingsModule : Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterType<Gibe.CacheBusting.RevisionManifest>().As<Gibe.CacheBusting.IRevisionManifest>();
+			builder.RegisterType<Gibe.CacheBusting.ConfigManifestFileFactory>().As<Gibe.CacheBusting.IManifestFileFactory>();
+			builder.RegisterType<Gibe.FileSystem.DirectoryService>().As<Gibe.FileSystem.IDirectoryService>();
+			builder.RegisterType<Gibe.FileSystem.FileService>().As<Gibe.FileSystem.IFileService>();
+			builder.RegisterType<Gibe.FileSystem.Paths>().As<Gibe.FileSystem.IPaths>();
+		}
+	}
+}
