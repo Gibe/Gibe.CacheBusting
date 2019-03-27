a66561437db8e83df4a1c0f758938df9fbd59579
Steve Temple <steve@gibedigital.com>
2019-03-27 22:32:03 +0000

    Put NetCoreServicesExtensions into correct namespace

--- Gibe.CacheBusting/NetCoreServicesExtensions.cs
+++ Gibe.CacheBusting/NetCoreServicesExtensions.cs
@@ -1,15 +1,12 @@
 ﻿#if NETCORE
-using Gibe.CacheBusting;
 using Gibe.FileSystem;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Gibe.CacheBusting2
-{
-
-	public static class NetCoreServicesExtensions
+namespace Gibe.CacheBusting
+{public static class NetCoreServicesExtensions
 	{
 		public static void AddCacheBusting(this IApplicationBuilder app)
 		{
